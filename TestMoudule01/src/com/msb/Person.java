70c2560046a7e10b2ed5da2dc35e203a87a3403e
ysm <1149906456@qq.com>
2021-06-23 23:11:58 +0800

    在student类中增加了属性

---
+++ TestMoudule01/src/com/msb/Person.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @Auther: ysm
+ * @Date: 2021/6/23
+ * @Description: com.msb
+ * @Version: 1.0
+ */
+public class Person {
+    private int age;
+    private String name;
+}
